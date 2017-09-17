a8689f9f2092bcb58dd5343bd4b86e47fedc9416
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-17 23:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0) {
-    console.log("Sorry, we don't have a credit card on file for you.")
-  } else {
+  if (cardNumber = Math.floor(Math.random() * 100000000)) {
+
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  } else {
+      console.log("Sorry, we don't have a credit card on file for you.")
   } return cart.length = 0
 }
