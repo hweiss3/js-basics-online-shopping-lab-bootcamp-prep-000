938e9d336398cdb5a8afcf783514f86efcbec179
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-17 20:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (cart = []) {
   for (var i = 0; i < cart.length; i++) {
     base += ` ${cart[i]} at $${itemName[itemPrice]}`
   }
-} return base
+} return console.log(base)
 }
 
 function total() {
