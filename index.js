539a0987c8eb32c783bb482e8cfe42010691f709
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-17 20:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemName = item
-  var item = {[itemName]: itemPrice}
   var itemPrice = Math.floor(Math.random() * 100)+1
+  var item = {[itemName]: itemPrice}
   cart.push(item)
   console.log(`${itemName} has been added to your cart.`)
   return cart
