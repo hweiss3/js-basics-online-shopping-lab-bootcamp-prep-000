33101c5732109a735b16063628190460b3f7b89a
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-17 23:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,10 +55,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber = Math.floor(Math.random() * 100000000)) {
-
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  if (cardNumber < 9999999 && cardNumber > 99999999) {
+    console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-      console.log("Sorry, we don't have a credit card on file for you.")
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } return cart.length = 0
 }
